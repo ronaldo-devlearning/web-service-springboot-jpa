f6513d3f3fee2d0565734d53c8db4c6d1e5473c8
Ronaldo Rocha <ronaldo.devlearning@gmail.com>
2022-10-29 17:50:58 -0300

    Service layer, component registration

---
+++ src/main/java/com/devlearning/web/services/UserService.java
@@ -0,0 +1,27 @@
+package com.devlearning.web.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devlearning.web.entities.User;
+import com.devlearning.web.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
